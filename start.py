6c79ddebc2a3e8edba2d24e19dd9472528b08aeb
Slavo Heys <47301102+slavo-heys@users.noreply.github.com>
2020-11-30 17:27:32 +0100

    Add files via upload

--- start.py
+++ start.py
@@ -12,33 +12,49 @@ class Example(QMainWindow):
 
     def initUI(self):
 
-        exitAct = QAction(QIcon('exit.png'), '&Zamknij', self)
-        exitAct.setShortcut('Ctrl+Q')
+    # ********** menu ustawienia
+        rejestracja = QAction(QIcon('icons/user.png'), '&Rejestracja użytkownika', self)
+        rejestracja.setStatusTip('Rejestracja nie jest przymusowa, jednak zarejestrowany użytkownik będzie miał więcej uprawnień')
+
+        lekarz = QAction(QIcon('icons/lekarz.png'), '&Rejestracja lekarza', self)
+        lekarz.setStatusTip('Dzięki rejestracji lekarza będziesz mógł wysyłać raporty pomiarów cukru swojemu lekarzowi')
+
+        exitAct = QAction(QIcon('icons/exit.png'), '&Zamknij', self)
         exitAct.setStatusTip('Zamknij program')
+        exitAct.setShortcut('Ctrl+Q')
         exitAct.triggered.connect(qApp.quit)
 
+    # ********** menu cukier
         poziomyAct = QAction(
-            QIcon('sugar-blood-level.png'), '&Dodaj pomiar', self)
-        poziomyAct.setShortcut('Ctrl+C')
+            QIcon('icons/sugar-blood-level.png'), '&Dodaj pomiar', self)
         poziomyAct.setStatusTip('Dodaj nowy pomiar poziomu cukru')
+        poziomyAct.setShortcut('Ctrl+D')
 
-        pokazPoziomy = QAction(QIcon('wyniki.png'),
+        pokazPoziomy = QAction(QIcon('icons/wyniki.png'),
                                '&Sprawdź moje pomiary', self)
-        pokazPoziomy.setShortcut('Ctrl+S')
         pokazPoziomy.setStatusTip('Wyświetl wszystkie poziomy cukru')
 
-        wagaZapisz = QAction(QIcon('waga.png'), '&Dodaj nową wagę', self)
+    # ************ menu waga
+        wagaZapisz = QAction(QIcon('icons/waga.png'), '&Dodaj nową wagę', self)
         wagaZapisz.setStatusTip(
             'Dodaj wagę ciała, aby mieć kontrolę nad masą ciała. Zalecane jest co tygodniowe ważenie.')
+        wagaZapisz.setShortcut('Ctrl+W')
 
-        wagaPokaz = QAction(QIcon('wynikiwazenia.png'),
+        wagaPokaz = QAction(QIcon('icons/wynikiwazenia.png'),
                             '&Pokaż wyniki ważenia', self)
         wagaZapisz.setStatusTip(
             'Sprawdź swoje wyniki ważenia, kontroluj wagę poprzez swoje BMI')
 
-        bmiPolicz = QAction(QIcon('bmi.png'), '&Policz BMI', self)
+        bmiPolicz = QAction(QIcon('icons/bmi.png'), '&Policz BMI', self)
         bmiPolicz.setStatusTip(
             'Sprawdź swoje BMI i bądź na bierząco z wynikami. Kontrola wagi przy cukrzycy jest bardzo ważna')
+        bmiPolicz.setShortcut('Ctrl+B')
+
+    # ************* menu raporty
+        raporty = QAction(QIcon('icons/raport.png'), '&Raport graficzny', self)
+        raporty.setStatusTip('Zobacz swoje graficzne raporty z zapisu wyników mierzenia cukru')
+
+        raportyDrukuj = QAction(QIcon('icons/drukuj.png'), '&Drukuj raport', self)
 
         self.statusBar()
 
@@ -52,12 +68,19 @@ class Example(QMainWindow):
         wagaMenu.addAction(wagaPokaz)
         wagaMenu.addAction(bmiPolicz)
 
-        fileMenu = menubar.addMenu('&File')
+        raportyMenu = menubar.addMenu('&Raporty')
+        raportyMenu.addAction(raporty)
+        raportyMenu.addAction(raportyDrukuj)
+
+        fileMenu = menubar.addMenu('&Ustawienia')
+        fileMenu.addAction(rejestracja)
+        fileMenu.addAction(lekarz)
         fileMenu.addAction(exitAct)
 
+    # *********** Geometria okna
         self.setGeometry(300, 300, 800, 600)
         self.setWindowTitle('Icon')
-        self.setWindowIcon(QIcon('sugar-blood-level.png'))
+        self.setWindowIcon(QIcon('icons/sugar-blood-level.png'))
         self.setWindowTitle('Simple menu')
         self.show()
 
